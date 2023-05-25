eea0ff42e995896c4160394554fa35a1d28c09f5
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2023-05-25 11:18:24 +0300

    feat: initial commit

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# **Plugin Name** Plugin
+# **Categoy Pin** Plugin
 
 **Plugin Summary**
 
-For more information, please see: **url to meta topic**
+This plugin is provide a simple featuer, where you can choose a category by `id`, and set how long each new topic created in that categoy to be pinned for.
+
+## Settings
+
+The settings can be changed from admin plugins dashboard,
+
+- category id, an integer of category which it's latest topic would be finned.
+- durattion (in minutes), an intenger for how long the category would be pinned.
+
+## Note
+
+The topic would be unpinned when either is true:
+
+- Pinning duration has passed.
+- A new topic with same catgory is posted, before the duration finish:
+  - i.e you post a topic and the setting is set to pinned for 60 minutes
+  - After 30 minutes you post a new topic with same category id
+    - The previous topic will be unpinned
+    - The new topic would be pinned for duration that set.
+
+- **The topic would be pinned globally**
--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-category-pin
+# about: discourse-category-pin
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Ghassan Maslamani 
+# url: discourse-category-pin
 # required_version: 2.7.0
-
-enabled_site_setting :plugin_name_enabled
-
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
-end
-
-require_relative "lib/my_plugin_module/engine"
-
+enabled_site_setting :category_pin_enabled
 after_initialize do
   # Code which should run after Rails has finished booting
+  DiscourseEvent.on(:topic_created) do |topic|
+    if SiteSetting.category_pin_id?
+        if topic.category_id?
+            if topic.category_id == SiteSetting.category_pin_id
+              # Check if there is exist topic
+              topics_pinned = Topic.where(category:SiteSetting.category_pin_id).where.not(pinned_at:nil)
+              topics_pinned.each do | pinned_topic |
+                pinned_topic.update_pinned(false)
+              end
+               # Pinngin the current topic
+               topic.update_pinned(true,gloabl=true, pinned_until=SiteSetting.category_pin_duration.minutes.from_now.to_s)
+            end
+        end
+    end     
+  end
 end
